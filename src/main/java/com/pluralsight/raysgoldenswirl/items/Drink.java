4bddfe0a771d493ca9e3d75c09a6de13e1ac259b
nati-tewolde <natisnewemail@gmail.com>
2025-11-12 21:48:04 -0500

    Add subclass Drink - Declare field size - Override and implement calculatePrice() and displayDetails()

---
+++ src/main/java/com/pluralsight/raysgoldenswirl/items/Drink.java
@@ -0,0 +1,32 @@
+package com.pluralsight.raysgoldenswirl.items;
+
+public class Drink extends Item {
+    private String size;
+
+    public Drink(String type, String size) {
+        super(type);
+        this.size = size;
+    }
+
+    @Override
+    public double calculatePrice() {
+        double basePrice = 0;
+
+        if (size.trim().equalsIgnoreCase("s")) {
+            basePrice = 2.00;
+        }
+        if (size.trim().equalsIgnoreCase("m")) {
+            basePrice = 2.50;
+        }
+        if (size.trim().equalsIgnoreCase("l")) {
+            basePrice = 3.00;
+        }
+
+        return basePrice;
+    }
+
+    @Override
+    public void displayDetails() {
+        System.out.printf("%s (%s) $%10.2f", getType(), size.toUpperCase(), calculatePrice());
+    }
+}
